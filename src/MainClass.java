5edd5aedd59b71d1040d05f69677bdd0222e21de
SyedAlibasha <syed.adil.12321abcd@gmail.com>
2023-04-01 14:31:58 +0530

    brickbreakinitial

---
+++ src/MainClass.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    // TODO Auto-generated method stub
+    JFrame f=new JFrame();
+        f.setTitle("Brick Breaker");
+        f.setSize(700,600);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setResizable(false);
+    GamePlay gameplay=new GamePlay();
+        f.add(gameplay);
+}
+}
